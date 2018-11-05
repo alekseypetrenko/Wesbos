f125530dab85ed536c0ac17bca9dc236d3df79d1
OlexiiPetrenko <olexii.petrenko@bazaarvoice.com>
2018-11-05 19:32:37 +0200

    order state

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -5,8 +5,9 @@ class Inventory extends React.Component {
     render() {
         return (
             <div className="inventory">
-                <h2>Inventory</h2>
+                <h2>Inventory2</h2>
                 <AddFishForm addFish = {this.props.addFish}/>
+                <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
             </div>
         )
     }
